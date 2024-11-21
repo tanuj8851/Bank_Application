d4031971200803bbbeff82f54da581b04d008728
Tanuj Kumar <tanujmaurya40@gmail.com>
2024-11-21 13:23:03 +0530

    Backend Url Updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://bankapplication-backend-qhkz.onrender.com/api",
+  // baseURL: "https://localhost:7000/api",
   withCredentials: true,
 });
 
 export default api;
 
 export const registerUser = (data) => api.post("/user/register", data);
-// export const logoutUser = () => api.post("/user/logout");
 export const depositMoney = (data) => api.post("/transactions/deposit", data);
 export const withdrawMoney = (data) => api.post("/transactions/withdraw", data);
 export const transferMoney = (data) => api.post("/transactions/transfer", data);
